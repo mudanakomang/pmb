7b075da7e191c1f769cbe958b171e840abb9cdb1
I Komang Mudana <mudanakomang@hotmail.com>
2020-12-04 18:14:37 +0800

    Major update

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pendaftaran Mahasiswa Baru</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <div class="main">
+
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container">
+                <div class="signup-header">
+                    <img src="images/header2020.png" alt="sing up image">
+                </div>
+                <div class="signup-content">
+
+                    <div class="signup-form">
+                        <!--<h2 class="form-title">Daftar</h2>-->
+                        <h3>Form Pendaftatan Mahasiswa Baru STKIP Suar Bangli</h3>
+                        <form method="POST" class="register-form" id="register-form" action="proses.php">
+                            <div class="form-group">
+                                <label for="nama"><i class="fa fa-user"></i></label>
+                                <input type="text" name="nama" id="nama" placeholder="Nama" />
+                            </div>
+                            <div class="form-group">
+                                <label for="nowa"><i class="fa fa-phone"></i></label>
+                                <input type="text" name="nowa" id="nowa" placeholder="Nomor WhatsApp" />
+                            </div>
+                            <div class="form-group">
+                                <label for="email"><i class="fa fa-envelope"></i></label>
+                                <input type="email" name="email" id="email" placeholder="Email" required/>
+                            </div>
+                            <div class="form-group">
+                                <label for="sekolah"><i class="fa fa-building"></i></label>
+                                <input type="text" name="sekolah" id="sekolah" placeholder="Sekolah Asal" required/>
+                            </div>
+                            <div class="form-group">
+                                <label for="alamat"><i class="fa fa-home"></i></label>
+                                <input type="text" name="alamat" id="alamat" placeholder="Alamat" required/>
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox" name="kip" id="kip" class="agree-term" />
+                                <label for="kip" class="label-agree-term"><span><span></span></span>Memiliki KIP (Kartu Indonesia Pintar) ? </label>
+                            </div>
+                            <div class="form-group" id="div_kip">
+                                <label for="nomorkip"><i class="fa fa-credit-card"></i></label>
+                                <input type="text" name="nomorkip" id="nomorkip" placeholder="Nomor KIP"/>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signup" id="signup" class="form-submit" onclick="event.preventDefault();formSubmit()" value="Daftar"/>
+                                <!--<a class="button" href="javascript:void(0);" onclick="submitForm()">Daftar</a>-->
+                            </div>
+                        </form>
+                    </div>
+                    <div class="signup-image">
+                        <figure><img src="images/projek1rev3.png" alt="sing up image"></figure>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/additional-methods.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+
+    <script>
+
+
+       $(function () {
+           var kip=$('#kip')
+           var kip_div=$('#div_kip')
+           kip_div.hide()
+           kip.change(function () {
+               if(kip.is(':checked')){
+                   kip_div.show()
+                   $('#nomorkip').rules('add',{required:true,messages:{required:"Nomor KIP tidak boleh kosong!"}})
+               }else{
+                   kip_div.hide()
+                   $('#nomorkip').rules('remove','required')
+                   $('#nomorkip').val('')
+               }
+           })
+       })
+    $().ready(function () {
+        $.validator.addMethod("regx", function(value, element, regexpr) {
+            var re = new RegExp(regexpr)
+            return re.test(value);
+        }, "Nomor WA tidak valid");
+        $('#register-form').validate({
+            rules:{
+                nama:"required",
+                nowa:{
+                    required:true,
+                    regx:'^(^\\+62\\s?|^0)(\\d{3,4}-?){2}\\d{3,4}$',
+
+                },
+                email:{
+                    required:true,
+                    email:true
+                },
+                sekolah:"required",
+                alamat:"required",
+            },
+            messages:{
+                nama:"Nama harus diisi!",
+                nowa:{
+                    required:"Nomor WA tidak boleh kosong!",
+                    regx:"Nomor WA tidak valid!"
+                },
+                email:{
+                    required:"Email tidak boleh kosong!",
+                    email:"Email tidak valid!"
+                },
+                sekolah:"Sekolah asal harus diisi!",
+                alamat:"Alamat harus diisi!"
+            }
+        })
+
+    })
+    function formSubmit() {
+        if(confirm("Anda akan dialihkan ke WhatsApp Admin STKIP Suar Bangli.")){
+            $.ajax({
+                url:'proses.php',
+                type:'POST',
+                data:{
+                    formdata:$('#register-form').serialize()
+                },success:function (s) {
+                    location.href=s.url
+                }
+            })
+        }
+    }
+    </script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
---
+++ proses.php
@@ -0,0 +1,49 @@
+<?php
+
+$servername = "localhost";
+$database = "pmb_stkipsuar";
+$username = "root";
+$password = "";
+
+$conn=mysqli_connect($servername,$username,$password,$database);
+if (!$conn){
+    die("Koneksi gagal: ".mysqli_connect_error());
+}
+
+$formdata=$_POST['formdata'];
+parse_str($formdata,$data);
+
+
+$nama=$data['nama'];
+$nowa=$data['nowa'];
+$email=$data['email'];
+$sekolah=$data['sekolah'];
+$alamat=$data['alamat'];
+$kip=isset($data['kip']) ? "Y":"N";
+$nomorkip=$data['nomorkip'];
+
+
+$stmt = $conn->prepare("INSERT INTO `pendaftar` (`nama`,`nomorwa`, `email`, `sekolah_asal`,`alamat`,`kip`,`nomor_kip`) VALUES (?,?,?,?,?,?,?)");
+$stmt->bind_param('sssssss',$nama,$nowa,$email,$sekolah,$alamat,$kip,$nomorkip);
+
+$stmt->execute();
+$stmt->close();
+$conn->close();
+
+$text = "Saya telah mendaftar melalui website Penerimaan Mahasiswa Baru STKIP Suar Bangli".PHP_EOL;
+$text.="dengan detail sebagai berikut:".PHP_EOL.PHP_EOL;
+$text.="*Nama:* ".$nama.PHP_EOL;
+$text.="*Nomor WhatsApp:* ".$nowa.PHP_EOL;
+$text.="*Email:* ".$email.PHP_EOL;
+$text.="*Sekolah Asal:* ".$sekolah.PHP_EOL;
+$text.="*Alamat:* ".$alamat.PHP_EOL;
+
+if (!empty($nomorkip)) {
+    $text .= "*KIP:*  " . $nomorkip . PHP_EOL;
+}
+$text=urlencode($text);
+$nomorhpadmin="6281238749696";
+$url="https://wa.me/".$nomorhpadmin.'?text='.$text;
+
+header('Content-Type: application/json');
+echo json_encode(['url'=>$url]);
